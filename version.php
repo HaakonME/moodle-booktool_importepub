bac795df021c261b5288b5de31e20937d7e01928
Eoin Campbell <campbeeo@tcd.ie>
2016-01-06 19:15:21 +0000

    Support splitting Word files into chapters using Heading 1 style as delimiter

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015122200;
+$plugin->version = 2016010601;
 $plugin->requires = 2012062500;
 $plugin->component = 'booktool_wordimport';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.0.2';
+$plugin->release = '0.1.0 (Build: 20160106)';
 
 $plugin->dependencies = array(
     'booktool_importhtml'     => 2012061700,
